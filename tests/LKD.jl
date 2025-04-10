1e8a581c2501186ea427b3dad800afbcdc24eff7
Alessio Quaresima <aquaresi@pasteur.fr>
2025-04-10 17:04:36 +0200

    monitor to monitor!

--- tests/LKD.jl
+++ tests/LKD.jl
@@ -16,13 +16,13 @@ IF_adex = SNN.AdEx(; N = 1, param = LKD2014.AdEx)
 
 plots = map([IF_pv, IF_adex]) do IF
     p1 = SNN.plot()
-    SNN.monitor([IF], [:fire, :v, :ge], sr=400Hz)
+    SNN.monitor!([IF], [:fire, :v, :ge], sr=400Hz)
     stim = SpikeTimeStimulus(IF, :ge, p=1.f0, param=inputs)
     sim!([IF],[SNN.EmptySynapse()], [stim], duration=1200ms, dt=0.125ms)
     label = "AMPA: $(round(IF.param.gsyn_e, digits=2))"
     p1 = SNN.vecplot!(p1, IF, :ge, r=800:0.1:1200ms, neurons=[1], dt=0.1ms,    label= label)
 
-    SNN.monitor([IF], [:fire, :v, :gi], sr=400Hz)
+    SNN.monitor!([IF], [:fire, :v, :gi], sr=400Hz)
     stim = SpikeTimeStimulus(IF, :gi, p=1.f0, param=inputs)
     sim!([IF],[SNN.EmptySynapse()], [stim], duration=1200ms, dt=0.125ms)
     label = "GABA: $(round(IF.param.gsyn_i, digits=2))"
